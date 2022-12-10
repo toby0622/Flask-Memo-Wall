c1e8efa82fe0647b830ecf7e9641b94dedec5907
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-12-10 21:04:50 +0800

    Logout Function Create

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -17,10 +18,19 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from . import models
+    from .models import User, Note
 
     # Flask-SQLALCHEMY 3 new method for creating database
     with app.app_context():
         db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    # inform Flask that which user we are looking for
+    @login_manager.user_loader
+    def load_user(input_id):
+        return User.query.get(int(input_id))
 
     return app
--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -17,6 +18,8 @@ def login():
         if user:
             if check_password_hash(user.password, password):
                 flash('Logged In Successfully!', category='success')
+                # remember the user login session until session closed
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect Password, Please Try Again.', category='error')
@@ -26,8 +29,11 @@ def login():
     return render_template("login.html")
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+
+    return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
@@ -56,6 +62,7 @@ def sign_up():
                             , password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash('Account Created!', category='success')
             return redirect(url_for('views.home'))
 

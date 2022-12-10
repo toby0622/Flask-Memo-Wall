8106bb8213b4c81dbc3cb56198799299e22e27c0
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-12-10 20:41:23 +0800

    Register Function Create

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -29,6 +32,11 @@ def sign_up():
             flash('Password Must Be At Least 7 Characters.', category='error')
         else:
             # add user to database
+            new_user = User(email=email, nickname=nickname
+                            , password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account Created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template("signup.html")

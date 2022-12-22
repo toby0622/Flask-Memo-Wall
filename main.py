230c86acb7b4cbb2e4f4e36619fc5f9cb2120636
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-12-22 09:41:33 +0800

    Comment Modification

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+# flask framework initialization
 from website import create_app
 
 app = create_app()
 
-# secure the web server to be executed
-# when actually needs, not accidentally
+# only called when itself needs to be executed
+# will not be called when itself be referenced
 if __name__ == '__main__':
     # automatically refresh the flask web server
     app.run(debug=True)

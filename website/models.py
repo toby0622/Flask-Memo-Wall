4fe9b0b65c84334bb18c8f928b6b7b986e3f049a
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-12-22 10:04:11 +0800

    Comment Modification

--- website/models.py
+++ website/models.py
@@ -1,12 +1,11 @@
 # schema definition
-# import from the current package
-# same as from website import db
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 # one to many situation (one guy having multiple notes)
 class Note(db.Model):
+    # primary key for identification
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
@@ -20,4 +19,5 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     nickname = db.Column(db.String(150))
+    # link a relationship with class "Note"
     notes = db.relationship('Note')

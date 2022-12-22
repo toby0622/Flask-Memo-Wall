d61e4b3a08e32c8cc75e5a9e0f953acd6a0b6c75
Chi-Wen "Toby" Teng <52705034+toby0622@users.noreply.github.com>
2022-12-22 09:51:23 +0800

    Comment Modification

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
 def create_app():
+    # application & database creation using Flask-SQLALCHEMY 3 module
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'NDHU410821316DatabaseSystem'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
@@ -20,7 +20,6 @@ def create_app():
 
     from .models import User, Note
 
-    # Flask-SQLALCHEMY 3 new method for creating database
     with app.app_context():
         db.create_all()
 
